24c181759d5056c0c7ec3d0b5e2cfb9ec3542f3a
Mohammed Serajuldeen <54007165+m2002k@users.noreply.github.com>
2025-04-24 21:28:33 +0300

    Update Scoreboard.jsx

---
+++ scoreboard-client/src/components/Scoreboard.jsx
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+
+function Scoreboard({ gameId }) {
+  return (
+    <div className="scoreboard">
+    </div>
+  );
+}
+
+export default Scoreboard;
